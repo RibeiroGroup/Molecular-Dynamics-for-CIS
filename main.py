852239c1368f979acc6e51cc2e2141294bf5e021
RibeiroGroup <hoangtuannguyen1507@gmail.com>
2023-12-06 13:08:40 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class mm_sim:
+    def __init__(self):
+
+    def force(self,x,v):
+
+    def integrate(self,f):
+
+    def sample(self):
+
+    def simulate(self):
+
