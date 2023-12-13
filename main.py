587a3855a9015ed632239202be3a8ad311fd13f9
Tuan <hoangtuannguyen1507@gmail.com>
2023-12-13 08:32:37 -0500

    Déc3th2023

--- main.py
+++ main.py
@@ -16,20 +16,28 @@ class vector_potential:
         + epsilon (numpy.ndarray): set of polarization vector
         Note that len(C) == len(k)
         """
-        assert len(C) == len(k)
-        assert len(epsilon) == len(k)
-
-        for v in epsilon:
-            assert len(v) == 2
+        assert C.shape[0] == k.shape[0]
 
         self.C = C
         self.k = k
+        self.V = V
 
     def __call__(self,x):
-        kx = np.dot(self.k, x)
-        result = np.dot(np.real(self.C), np.cos(kx)) \
-                - np.dot(np.imag(self.C), np.sin(kx))
-        return result / V**(0.5)
+        kx = k @ x
+        kx = np.tile(kx.reshape(-1,1),(1,2))
+
+        """
+        Ckx = 2 * (np.real(C) * np.cos(kx) - np.imag(C) * np.sin(kx))
+        """
+        Ckx = C * np.exp(kx * np.array(1.j))
+        Ckx += np.conjugate(C) * np.exp(kx * np.array(-1.j))
+
+        Ckx = Ckx.reshape(*epsilon.shape[:-1], -1)
+        Ckx = epsilon * Ckx 
+
+        result = np.sum(Ckx, axis = 0)
+
+        return result / self.V**(0.5)
 
     def curl(self,x):
         raise Exception("To be implemented")
@@ -47,28 +55,64 @@ class EMfield:
     def __init__(self, k_vectors):
         self.k_list = k_vector
 
+def A_test_case(single = False, seed = None):
+    if seed: np.random.seed(seed)
+    C = np.array([
+        [0.5 + 1.j, 1 + 0.5j],
+        [1 + 2.j, 2 + 1.j],
+        [3 + 4.j, 4 + 3.j],
+        [5 + 6.j, 6 + 5.j],
+        ])
 
-V = 1
+    k = np.array([
+        [1,0,0],
+        [0,1,0],
+        [0,0,1],
+        [0,1,0],
+        ]) # N x 3
 
-C = np.array([
-    1 + 2.j, 
-    2 + 1.j
-    ])
-k = np.array([
-    [1,0,0],
-    [0,1,0]
-    ])
-epsilon = np.array([
-    [[0,1,0] ,[0,0,1]],
-    [[1,0,0] ,[0,0,1]]
-    ])
+    epsilon = np.array([
+        [[0,1,0] ,[0,0,1]],
+        [[1,0,0] ,[0,0,1]],
+        [[1,0,0] ,[0,1,0]],
+        [[1,0,0] ,[0,0,1]],
+        ])
+    
+    if single:
+        idx = np.random.randint(low = 0, high = len(C), size = 3)
 
-x = np.array([0.1,0.2,0.3])
-A = vector_potential(C=C, k=k)
+        return C[idx[0],:].reshape(1,-1), \
+            k[idx[1]].reshape(1,-1), epsilon[idx[2]]
 
+    return C, k, epsilon
 
+x = np.array([0.1,0.2,0.3]) # 3
+C,k,epsilon = A_test_case(True, 2022)
 
+A = vector_potential(C=C, k=k, epsilon=epsilon)
+print(A(x))
 
+kx = k @ x
+kx = np.tile(kx.reshape(-1,1),(1,2))
 
+"""
+Ckx = 2 * (np.real(C) * np.cos(kx) - np.imag(C) * np.sin(kx))
+"""
+Ckx = C * np.exp(kx * np.array(1.j))
+Ckx += - np.conjugate(C) * np.exp(kx * np.array(-1.j))
+Ckx *= np.array(1.j)
+
+Ckx = Ckx.reshape(*epsilon.shape[:-1], -1)
+Ckx = epsilon * Ckx 
+
+Ckx = np.sum(Ckx, axis = 1)
+print(Ckx)
+
+dAdr = []
+for i in range(len(Ckx)):
+    dAdr.append(np.outer(k[i,:], Ckx[i,:] ))
+dAdr = np.array(dAdr)
+
+print(dAdr)
 
 
