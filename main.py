6697cf41624976a1e566e521eb00d69efa920c2a
RibeiroGroup <hoangtuannguyen1507@gmail.com>
2023-12-14 16:57:33 -0500

    n

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import numpy as np
 
-from MEfield import vector_potential
+from EM_field import vector_potential
+from num_pde import RK4
 from test_cases import A_test_case
 
-x = np.array([0.1,0.2,0.3]) # 3
+np.random.seed(2022)
 
-C,k,epsilon = A_test_case()#True, 2022)
-print(C,k,epsilon)
+k_vec = np.array([1,0,0])
+epsilon = np.array([
+    [0, 1, 0], [0, 0, 1]
+    ])
+C0 = np.random.rand(size=2) +1j *  np.random.rand(size=2)
 
-A = vector_potential(C=C, k=k, epsilon=epsilon)
-print(A.diff_ra(x))
+c = 1.37036e2
+k = np.sqrt(k_vec @ k_vec)
+omega = c * k
+
+x = np.array([0.1,0.2,0.3])
+
+ra = np.random.rand(3)
+va = np.random.rand(3)
+
