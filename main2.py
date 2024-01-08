9c4518e62ed39f12346d944816e905842fd4a57e
RibeiroGroup <hoangtuannguyen1507@gmail.com>
2024-01-08 16:24:57 -0500

    Jan 6th 2024

--- main2.py
+++ main2.py
@@ -9,9 +9,9 @@ from Charge import ChargePoint, ChargeCluster
 
 import constants
 
-np.random.seed(2024)
+np.random.seed(20)
 # FIELD SPECS
-k_ = 2 * np.pi / (700e-9 / constants.a0)
+k_ = 2 * np.pi / (100e-9 / constants.a0)
 A = MultiModeField(
     C= (np.random.rand(2) + 1j * np.random.rand(2)),
     k=np.array([k_,0,0]),
@@ -21,14 +21,14 @@ A = MultiModeField(
 #PARTICLE SPECS 
 alpha = ChargePoint(
         m = 1, q = 1, 
-        r = np.zeros(3), # np.random.rand(3), # 
+        r = np.random.rand(3), # np.zeros(3), #
         v = np.random.rand(3), # np.zeros(3), # 
         )
 
 beta = ChargePoint(
         m = 1, q = 1, 
-        r = np.random.rand(3), # np.zeros(3), # 
-        v = np.random.rand(3), # np.zeros(3), # 
+        r = np.zeros(3), # np.random.rand(3), #
+        v = np.random.rand(3), # np.zeros(3) #
         )
 
 # EXPLICIT CALCULATION
@@ -40,7 +40,7 @@ def dot_C(q, r, v, k_vec, C, epsilon):
 
     jk = 0
     for i,qi in enumerate(q):
-        jk += np.exp(-1j * k_vec @ r[i]) * qi * v[i]# * (1 * np.pi)**(-1.5)
+        jk += np.exp(-1j * k_vec @ r[i]) * qi * v[i] # * (1 * np.pi)**(-1.5)
 
     jk_transv = (np.eye(3) - np.outer(k_vec, k_vec) / (k**2)) @ jk
     proj_jk_transv = np.array([
@@ -103,10 +103,11 @@ def compute_long_force(q, r, v, k_vec, C, epsilon):
 
     return np.array(ma_list)
 
-def compute_force(q, r, v, k_vec, C, epsilon):
-    F_transv = compute_transv_force(q, r, v, k_vec, C, epsilon)
-    F_long = compute_long_force(q, r, v, k_vec, C, epsilon)
-    return F_transv + F_long
+def compute_oscillator_force(r, k_const):
+    ma = []
+    for ri in r:
+        ma.append( - k_const * ri)
+    return np.array(ma)
 
 def compute_Hmat_transv(q,r,v):
     K = 0
@@ -133,6 +134,26 @@ def compute_Hem(k_vec,C):
     return (2 * np.pi)**-1 * (k_vec @ k_vec.T) \
         * (C @ np.conjugate(C).T) 
 
+def compute_H_oscillator(r,k_const):
+    K = 0
+    for ri in r:
+        K += 0.5 * k_const * (ri @ ri.T).item()
+    return K
+
+def compute_force(q, r, v, k_vec, C, epsilon, k_const=None):
+    F_transv = compute_transv_force(q, r, v, k_vec, C, epsilon)
+    F_long = compute_long_force(q, r, v, k_vec, C, epsilon)
+    F_oscillator = compute_oscillator_force(r,k_const) \
+        if k_const!=None else 0
+    return F_transv + F_long + F_oscillator
+
+def compute_H(q, r, v, k_vec, C, epsilon, k_const=None):
+    Hem = compute_Hem(k_vec, C)
+    Hmat = compute_Hmat(q, r, v)
+    if k_const != None:
+        Hmat += compute_H_oscillator(r,k_const)
+    return Hem, Hmat
+
 print("####### Initial field parameters value #######")
 C = A.C[0]
 print("C = ",C)
@@ -140,62 +161,73 @@ k_vec = deepcopy(A.k[0])
 print("k = ",k_vec)
 epsilon = np.array(A.epsilon[0])
 print("epsilon = ",epsilon)
-h = 1e-2
+h = 5e-2
 print("h = ", h)
 
 print("### Initial charge point parameters value ###")
-q = [beta.q, alpha.q]
+q = [beta.q,]# alpha.q]
 print("q = ",q)
-r = np.vstack([beta.r, alpha.r])
+r = np.vstack([beta.r])#, alpha.r])
 r = r.reshape(-1,3)
 print("r = ",r)
-v = np.vstack([beta.v, alpha.v])
+v = np.vstack([beta.v])#, alpha.v])
 v = v.reshape(-1,3)
 print("v = ",v)
 
+print("############# others #############")
+
+k_const = 1
+print("k_const",k_const)
+
 print("#############################################")
 
-em_H_list = [compute_Hem(k_vec, C)]
-mat_H_list = [compute_Hmat(r=r,v=v,q=q)]
-H_list = [compute_Hem(k_vec, C) + compute_Hmat(r=r,v=v,q=q)]
+Hem, Hmat = compute_H(
+        q=q,r=r,v=v,k_vec=k_vec,C=C,epsilon=epsilon,k_const=k_const)
+
+print("Hamiltonian: {}(field) + {}(matter)".format(Hem,Hmat))
+
+em_H_list = [Hem]
+mat_H_list = [Hmat]
+H_list = [Hem + Hmat]
 steps_list = [0]
 
-trajectory = {"r":[r], "v":[v]}
-hamiltonian = {"em":[compute_Hem(k_vec, C)], "mat":[compute_Hmat(r=r,v=v,q=q)]}
+trajectory = {"initial":{"q":q,"r":r,"v":v,"k_const":k_const},
+        "r":[r], "v":[v]}
+hamiltonian = {"em":[Hem], "mat":[Hmat]}
 
-for i in range(int(1e3+1)):
+for i in range(int(3e2+1)):
     k1c = dot_C(
         q=q, r=r, v=v, C=C, k_vec=k_vec, epsilon=epsilon)
     k1v = compute_force(
-        q=q, r=r, v=v, C=C, k_vec=k_vec, epsilon=epsilon)
+        q=q, r=r, v=v, C=C, k_vec=k_vec, epsilon=epsilon,k_const=k_const)
     k1r = v
 
     k2c = dot_C(
         q=q, r=r+k1r*h/2, v=v+k1v*h/2, C=C+k1c*h/2, k_vec=k_vec, epsilon=epsilon)
     k2v = compute_force(
-        q=q, r=r+k1r*h/2, v=v+k1v*h/2, C=C+k1c*h/2, k_vec=k_vec, epsilon=epsilon)
+        q=q, r=r+k1r*h/2, v=v+k1v*h/2, C=C+k1c*h/2, k_vec=k_vec, epsilon=epsilon,k_const=k_const)
     k2r = v + k1v*h/2
 
     k3c = dot_C(
         q=q, r=r+k2r*h/2, v=v+k2v*h/2, C=C+k2c*h/2, k_vec=k_vec, epsilon=epsilon)
     k3v = compute_force(
-        q=q, r=r+k2r*h/2, v=v+k2v*h/2, C=C+k2c*h/2, k_vec=k_vec, epsilon=epsilon)
+        q=q, r=r+k2r*h/2, v=v+k2v*h/2, C=C+k2c*h/2, k_vec=k_vec, epsilon=epsilon,k_const=k_const)
     k3r = v + k2v*h/2
 
     k4c = dot_C(
         q=q, r=r+k3r*h, v=v+k3v*h, C=C+k3c*h, k_vec=k_vec, epsilon=epsilon)
     k4v = compute_force(
-        q=q, r=r+k3r*h, v=v+k3v*h, C=C+k3c*h, k_vec=k_vec, epsilon=epsilon)
+        q=q, r=r+k3r*h, v=v+k3v*h, C=C+k3c*h, k_vec=k_vec, epsilon=epsilon,k_const=k_const)
     k4r = v + k3v*h
 
     r = r + (h/6) * (k1r + 2*k2r + 2*k3r + k4r)
     v = v + (h/6) * (k1v + 2*k2v + 2*k3v + k4v)
     C = C + (h/6) * (k1c + 2*k2c + 2*k3c + k4c)
 
-    H_mat = compute_Hmat(r=r,v=v,q=q)# * constants.c
-    mat_H_list.append(H_mat)
+    H_em, H_mat = compute_H(
+        q=q,r=r,v=v,k_vec=k_vec,C=C,epsilon=epsilon,k_const=k_const)
 
-    H_em = compute_Hem(k_vec, C) 
+    mat_H_list.append(H_mat)
     em_H_list.append(H_em)
 
     H_list.append(H_mat + H_em)
@@ -206,7 +238,7 @@ for i in range(int(1e3+1)):
     hamiltonian["mat"].append(H_mat)
 
     steps_list.append(i)
-    if i % 1e2 == 0:
+    if i % 1e1 == 0:
         print("Step {}".format(i+1))
         print("r = ",r)
         print("v = ",v)
@@ -221,16 +253,20 @@ for i in range(int(1e3+1)):
 fig, ax = plt.subplots(3)
 
 ax[0].plot(steps_list, em_H_list)
+ax[0].set_ylabel(r"$H_{field}$")
 
 ax[1].plot(steps_list, mat_H_list)
+ax[1].set_ylabel(r"$H_{matter}$")
 
 ax[2].plot(steps_list, H_list)
 ax[2].set_ylim(np.array([-0.5,0.5]) + np.mean(H_list))
+ax[2].set_xlabel("Time steps")
+ax[2].set_ylabel(r"$H_{total}$")
 
-fig.savefig("result_plot\\particle_field_energy.jpeg",dpi=600)
+fig.savefig("result_plot/particle_field_energy.jpeg",dpi=600)
 
-with open("result_plot\\trajectory.pkl","wb") as handle:
+with open("result_plot/trajectory.pkl","wb") as handle:
     pickle.dump(trajectory,handle)
 
-with open("result_plot\\hamiltonian.pkl","wb") as handle:
+with open("result_plot/hamiltonian.pkl","wb") as handle:
     pickle.dump(hamiltonian,handle)
