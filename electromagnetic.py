9b1e054efdf67583113ca9f1987552153c878830
RibeiroGroup <hoangtuannguyen1507@gmail.com>
2024-04-04 16:18:02 -0400

    April 4th 2024

--- electromagnetic.py
+++ electromagnetic.py
@@ -1,10 +1,53 @@
 import numpy as np
+import numpy.linalg as la
+
+from utils import PBC_wrapping, orthogonalize
+
+run_test = True
 
 class VectorPotential:
-    def __init__(self, kvec, amplitudes, polarization_vector):
+    def __init__(self, k_vector, amplitudes):
+        """
+        Args:
+        + k_vector (np.array): should have shape N_modes x 3 x 3
+            where the 1st dim -> mode, 2nd dim -> k vector, 1st & 2nd 
+            polarization vector and the last dim is vector length (coordinate)
+        """
 
-        assert kvec.shape[1] == 3 and len(kvec.shape) == 2
-        self.n_modes = kvec.shape[0]
-        self.kvec_list = np.array(kvec)
+        self.N_modes = len(k_vector)
 
-        assert amplitudes.shape == 
+        if kvec.shape == (self.N_modes, 3, 3):
+            self.k_vector = k_vector
+        else:
+            assert k_vector.shape == (N_modes, 3)
+            self.k_vector = np.array([
+                orthogonalize(kvec) for kvec in k_vector
+                ]) # each k_vector should have shape N_mode x 3 x 3
+
+        assert amplitudes.shape == (N_modes, 2)
+
+        self.C = amplitudes
+
+    def __call__(self, R):
+        k_vec = k_vector[:,0,:]
+        pol_vec = k_vec[:,1:,:]
+        
+        f_R = np.einsum("kj,ij->ik",k_vec,R)
+
+    def gradient(self, R):
+        pass
+
+    def dot(self, R):
+        pass
+
+if run_test == True:
+    k_vector = np.array([
+            [1,0,0],
+            [1,1,0]
+            ], dtype= np.float64)
+
+    k_vector = np.array([
+        orthogonalize(kvec) for kvec in k_vector
+        ]) 
+
+    print(k_vector[:,0,:].shape)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+import numpy.linalg as la
 
 def timeit(func):
     def inner(*args, **kwargs):
@@ -18,95 +19,34 @@ def PBC_wrapping(r, L):
         r = np.where(r < -L/2, r + L, r)
     return r
 
-def get_dist_matrix(distance_vector):
-
-    distance_matrix = np.sqrt(
-        np.sum(distance_vector**2, axis = -1))
+def orthogonalize(vec, eps=1e-15):
+    """
+    Orthogonalizes the matrix U (d x n) using Gram-Schmidt Orthogonalization.
+    If the columns of U are linearly dependent with rank(U) = r, the last n-r columns 
+    will be 0.
     
-    return distance_matrix
-
-"""
-L = 100
-n_points = 10
-all_r = np.random.uniform(-L/2,L/2,size=(n_points,3))
-
-dc = DistanceCalculator(5, box_length= L)
-dc_ = DistanceCalculator(n_points, box_length= L)
-rvec = dc.get_distance2(all_r[5:], all_r[:5])
-
-foo = dc_(all_r)
-foo = foo[:5,5:,:][dc.utriang_bool_mat_x3].reshape(-1,3)
-
-print(rvec + foo)
-"""
-
-def test_for_distance_matrix(ra):
-    n_points = ra.shape[0]
-    dist_mat_ = np.zeros((n_points,n_points))
-    for i, ri in enumerate(ra):
-        for j,rj in enumerate(ra):
-            if i == j: continue
-            #j_ = j if j < i else j-1
-            dist_mat_[i][j] = np.sqrt(np.sum((ri - rj)**2))
-
-    return dist_mat_
-
-def test_for_distance_vector(x):
-    n_points = x.shape[0]
-
-    d_ = np.zeros((n_points, n_points, 3))
-    for i, x1 in enumerate(x):
-        for j, x2 in enumerate(x):
-            if i == j: continue
-            d_[i,j,:] = x1 - x2
-
-    return d_
-
-
-"""
-n_points = 10
-all_r = np.random.rand(n_points,3)
-distant_calc = DistanceCalculator(n_points)
-
-rvec = distant_calc(all_r)
-
-rvec_test = test_for_distance_vector(all_r)
-
-print(rvec - rvec_test)
-
-"""
-"""
-@PBC_decorator(L = L)
-def get_dist_vector(R):
-    rij_vec_tensor = np.zeros((n_points, n_points,3))
-    for i in range(len(R)):
-        for j in range(i, len(R)):
-            
-            ri = R[i]; rj = R[j];
-
-            rij_vec = ri - rj
-            #rij = np.sqrt(np.sum((ri - rj)**2))
-
-            rij_vec_tensor[i,j,:] = rij_vec
-            rij_vec_tensor[j,i,:] = - rij_vec
-
-    return rij_vec_tensor
-
-def PBC_wrapping(r, L):
-    r = np.where(r >= L/2, r - L, r)
-    r = np.where(r < -L/2, r + L, r)
-    return r
-
-def PBC_decorator(L):
-    def real_PBC_wrap(func):
-        def PBC_wrapping_wrapper(*args,**kwargs):
-            dist = func(*args, **kwargs)
-            dist = PBC_wrapping(dist, L)
-
-            return dist
-
-        return PBC_wrapping_wrapper
-    return real_PBC_wrap
-
-
-"""
+    Args:
+        U (numpy.array): A d x n matrix with columns that need to be orthogonalized.
+        eps (float): Threshold value below which numbers are regarded as 0 (default=1e-15).
+    
+    Returns:
+        (numpy.array): A d x n orthogonal matrix. If the input matrix U's cols were
+            not linearly independent, then the last n-r cols are zeros.
+    """
+    
+    U = np.vstack([vec, np.random.rand(2,3)])
+    
+    n = len(U[0])
+    # numpy can readily reference rows using indices, but referencing full rows is a little
+    # dirty. So, work with transpose(U)
+    V = U.T
+    for i in range(n):
+        prev_basis = V[0:i]     # orthonormal basis before V[i]
+        coeff_vec = np.dot(prev_basis, V[i].T)  # each entry is np.dot(V[j], V[i]) for all j < i
+        # subtract projections of V[i] onto already determined basis V[0:i]
+        V[i] -= np.dot(coeff_vec, prev_basis).T
+        if la.norm(V[i]) < eps:
+            V[i][V[i] < eps] = 0.   # set the small entries to 0
+        else:
+            V[i] /= la.norm(V[i])
+    return V.T
