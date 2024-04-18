cd59d7e80292b932163c6c7bb7e1f42f5cc63760
RibeiroGroup <hoangtuannguyen1507@gmail.com>
2024-04-18 17:03:08 -0400

    Apr 18th 2024

--- plot.py
+++ plot.py
@@ -1,3 +1,4 @@
+from math import floor
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,14 +9,20 @@ with open("result_plot/trajectory_temp.pkl","rb") as handle:
 
 kinetic_energy = np.array(trajectory["kinetic_energy"])
 potential_energy = np.array(trajectory["potential_energy"])
+
 rad_energy = np.array(trajectory["EM_energy"])
-hamiltonian = kinetic_energy + potential_energy + rad_energy
+print(rad_energy.shape)
+total_rad_energy = np.sum(rad_energy,axis = 1)
 
-plot_range = slice(0, len(trajectory["time"]))
+hamiltonian = kinetic_energy + potential_energy + total_rad_energy
 
-t = np.array(trajectory["time"][plot_range])
+t = np.array(trajectory["time"])
+#t = t[t < 3.0]
+
+plot_range = slice(0, len(t))
 
 fig, ax = plt.subplots(3,2,figsize = (12,12))
+
 ax[0,0].plot(t, trajectory["total dipole"][plot_range])
 ax[0,0].set_xlabel("Time")
 ax[0,0].set_ylabel("Total dipole")
@@ -24,7 +31,7 @@ ax[0,1].plot(t, kinetic_energy[plot_range])
 ax[0,1].set_xlabel("Time")
 ax[0,1].set_ylabel("Kinetic energy")
 
-ax[1,0].plot(t, rad_energy[plot_range])
+ax[1,0].plot(t, total_rad_energy[plot_range])
 ax[1,0].set_xlabel("Time")
 ax[1,0].set_ylabel("EM_energy")
 
@@ -37,3 +44,11 @@ ax[2,1].set_xlabel("Time")
 ax[2,1].set_ylabel("Hamiltonian")
 
 fig.savefig("result_plot/hamiltonians.jpeg", bbox_inches="tight",dpi=600)
+
+
+fig, ax = plt.subplots(5,3,figsize = (20,12))
+
+for i in range(rad_energy.shape[1]):
+    ax[i%5,floor(i/5)].plot(t, rad_energy[plot_range,i])
+
+fig.savefig("result_plot/em.jpeg",bbox_inches ="tight", dpi = 600)
